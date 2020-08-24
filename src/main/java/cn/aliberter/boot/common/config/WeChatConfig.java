fa499faabd638efd3b59006583826e3841f70f92
wangsj <itwangshijie@126.com>
2020-08-24 15:10:50 +0800

    feat(集成微信公众号开发框架):

--- src/main/java/cn/aliberter/boot/common/config/WeChatConfig.java
+++ src/main/java/cn/aliberter/boot/common/config/WeChatConfig.java
@@ -2,15 +2,12 @@ package cn.aliberter.boot.common.config;
 
 import cn.aliberter.boot.common.properties.CustomSetting;
 import lombok.AllArgsConstructor;
-import me.chanjar.weixin.common.redis.RedisTemplateWxRedisOps;
 import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
 import me.chanjar.weixin.mp.config.impl.WxMpDefaultConfigImpl;
-import me.chanjar.weixin.mp.config.impl.WxMpRedisConfigImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * @author : aliberter
@@ -20,14 +17,11 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @AllArgsConstructor
 public class WeChatConfig {
 
-    private final StringRedisTemplate stringRedisTemplate;
-
     @Bean
     public WxMpService wxMpService() {
 
         WxMpService service = new WxMpServiceImpl();
-        WxMpDefaultConfigImpl configStorage = new WxMpRedisConfigImpl(new RedisTemplateWxRedisOps(
-                stringRedisTemplate), CustomSetting.getStr("wechat.appId"));
+        WxMpDefaultConfigImpl configStorage = new WxMpDefaultConfigImpl();
         configStorage.setAppId(CustomSetting.getStr("wechat.appId"));
         configStorage.setSecret(CustomSetting.getStr("wechat.secret"));
         configStorage.setToken(CustomSetting.getStr("wechat.token"));
